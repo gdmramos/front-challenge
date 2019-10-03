4882f8fb90f44f6219cd5eca5045d3b845611b0a
Gabriel de Miranda Ramos <gabrieldemiranda.ramos@hotmail.com>
2019-10-03 04:04:36 -0300

    adding select component

---
+++ components/Select.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-select :options="options" placeholder="Filter by Region" />
+</template>
+<script>
+import vSelect from 'vue-select'
+
+export default {
+  components: {
+    vSelect
+  },
+  data () {
+    return {
+      options: [
+        'Africa',
+        'America',
+        'Asia',
+        'Europe',
+        'Oceania'
+      ]
+    }
+  }
+}
+</script>
