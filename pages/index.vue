2cb5dc73048ef015bd83a4b859f4442df7149bc3
Gabriel de Miranda Ramos <gabrieldemiranda.ramos@hotmail.com>
2019-10-03 20:51:26 -0300

    layout styles and grid

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="container">
-    <search />
-    <v-select />
+  <div class="index">
+    <section class="input-wrappers">
+      <search />
+      <v-select />
+    </section>
+    <section class="card-wrappers">
+      <card />
+      <card />
+      <card />
+      <card />
+    </section>
   </div>
 </template>
 
 <script>
 import Search from '@/components/Search'
 import vSelect from '@/components/Select'
+import Card from '@/components/Card'
 
 export default {
   components: {
     Search,
-    vSelect
+    vSelect,
+    Card
   }
 }
 </script>
