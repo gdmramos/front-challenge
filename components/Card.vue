238f6a18797b0d61d663f0cb304876564465716c
Gabriel de Miranda Ramos <gabrieldemiranda.ramos@hotmail.com>
2019-10-03 20:46:25 -0300

    card style

---
+++ components/Card.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="card">
+    <div class="card-flag">
+      <img src="@/static/germany.png" alt="">
+    </div>
+    <div class="card-content">
+      <h2>{{ country.name }}</h2>
+      <p><span>Population: </span>{{ country.population }}</p>
+      <p><span>Region: </span>{{ country.region }}</p>
+      <p><span>Capital: </span>{{ country.capital }}</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      country: {
+        name: 'Germany',
+        population: '81.770.900',
+        region: 'Europe',
+        capital: 'Berlin'
+      }
+    }
+  }
+}
+</script>
