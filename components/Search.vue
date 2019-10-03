6d070551f68ef271376a2f71781cc7cb2c192752
Gabriel de Miranda Ramos <gabrieldemiranda.ramos@hotmail.com>
2019-10-03 04:04:10 -0300

    adding search component

---
+++ components/Search.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="search">
+    <ion-icon class="search-icon" name="search" />
+    <input class="search-input" type="text" placeholder="Search for a country">
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
