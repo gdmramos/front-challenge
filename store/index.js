c1c65fd32f358cbb6fdf686ecf4a9413001ff9c5
Gabriel de Miranda Ramos <gabrieldemiranda.ramos@hotmail.com>
2019-10-05 15:39:41 -0300

    caching requests on Vuex

--- store/index.js
+++ store/index.js
@@ -1,14 +1,30 @@
 export const state = () => ({
   countriesList: [],
-  selectedFilter: null
+  selectedCountry: [],
+  bordersList: [],
+  regionFilter: null,
+  countryFilter: '',
+  countryIsLoaded: false
 })
 
 export const mutations = {
   addList (state, data) {
     state.countriesList = data
   },
-  setFilter (state, data) {
-    state.selectedFilter = data
+  setCountry (state, data) {
+    state.selectedCountry = data
+  },
+  setBorders (state, data) {
+    state.bordersList = data
+  },
+  setRegionFilter (state, data) {
+    state.regionFilter = data
+  },
+  setCountryFilter (state, data) {
+    state.countryFilter = data
+  },
+  setCountryIsLoaded (state, data) {
+    state.countryIsLoaded = data
   }
 }
 
@@ -16,5 +32,28 @@ export const actions = {
   async fetchList ({ commit }) {
     const data = await this.$axios.$get('/all?fields=name;region;population;capital;flag')
     commit('addList', data)
+  },
+
+  async fetchCountry ({ commit, state }, country) {
+    if (state.selectedCountry.length > 0) {
+      if (state.selectedCountry[0].name !== country) {
+        const data = await this.$axios.$get(`/name/${country}`)
+        commit('setCountry', data)
+        commit('setCountryIsLoaded', true)
+      } else {
+        commit('setCountryIsLoaded', true)
+      }
+    } else {
+      const data = await this.$axios.$get(`/name/${country}`)
+      commit('setCountry', data)
+      commit('setCountryIsLoaded', true)
+    }
   }
+  // async fetchBorders ({ commit }) {
+  //   const borderArray = []
+  //   countries.forEach((country) => {
+  //     const data = await this.$axios.$get(`/name/${this.$route.params.country}`)
+  //     borderArray.push()
+  //   })
+  // }
 }
