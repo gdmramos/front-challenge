43ba3ac95b2181282ca288de7503673bc9ed4f26
Gabriel de Miranda Ramos <gabrieldemiranda.ramos@hotmail.com>
2019-10-05 15:39:07 -0300

    search filte

--- components/Search.vue
+++ components/Search.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="search">
     <ion-icon class="search-icon" name="search" />
-    <input class="search-input" type="text" placeholder="Search for a country...">
+    <input v-model="value" class="search-input" type="text" placeholder="Search for a country..." @input="setCountryFilter">
   </div>
 </template>
 <script>
 export default {
-
+  data () {
+    return {
+      value: ''
+    }
+  },
+  methods: {
+    setCountryFilter () {
+      this.$store.commit('setCountryFilter', this.value)
+    }
+  }
 }
 </script>
