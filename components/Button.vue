3ee6a2162c16f99295fa9b723eddee5fdf54a988
Gabriel de Miranda Ramos <gabrieldemiranda.ramos@hotmail.com>
2019-10-05 13:35:00 -0300

    button component

---
+++ components/Button.vue
@@ -0,0 +1,16 @@
+<template>
+  <nuxt-link :to="path" class="button">
+    <slot />
+  </nuxt-link>
+</template>
+<script>
+export default {
+  props: {
+    path: {
+      type: String,
+      required: false,
+      default: '/'
+    }
+  }
+}
+</script>
