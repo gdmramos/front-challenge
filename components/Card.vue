16aeaa9839011ddef40ddb5af82ecdaa0ab86a5a
Gabriel de Miranda Ramos <gabrieldemiranda.ramos@hotmail.com>
2019-10-05 13:37:01 -0300

    card integration

--- components/Card.vue
+++ components/Card.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="card">
     <div class="card-flag">
-      <img src="@/static/germany.png" alt="">
+      <img :src="country.flag" alt="country flag">
     </div>
     <div class="card-content">
       <h2>{{ country.name }}</h2>
@@ -13,13 +13,28 @@
 </template>
 <script>
 export default {
+  props: {
+    country: {
+      type: Object,
+      default () {
+        return {
+          name: 'Germany',
+          population: '81.770.900',
+          region: 'Europe',
+          capital: 'Berlin',
+          flag: 'https://restcountries.eu/data/afg.svg'
+        }
+      }
+    }
+  },
   data () {
     return {
-      country: {
+      data: {
         name: 'Germany',
         population: '81.770.900',
         region: 'Europe',
-        capital: 'Berlin'
+        capital: 'Berlin',
+        flag: 'https://restcountries.eu/data/afg.svg'
       }
     }
   }
